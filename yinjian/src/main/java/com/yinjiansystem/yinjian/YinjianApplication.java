86c79ca0306a3ddfe65864727b07b56586aa3172
Li Zuxi <775717876@qq.com>
2020-11-26 16:09:57 +0800

    项目后端工程

---
+++ yinjian/src/main/java/com/yinjiansystem/yinjian/YinjianApplication.java
@@ -0,0 +1,13 @@
+package com.yinjiansystem.yinjian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YinjianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YinjianApplication.class, args);
+	}
+
+}
---
+++ yinjian/src/test/java/com/yinjiansystem/yinjian/YinjianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yinjiansystem.yinjian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YinjianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
