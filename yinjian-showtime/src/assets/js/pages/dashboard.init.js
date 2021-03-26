8e427a17e617eaa5b8a47aa3b9d1b2e345309d8a
Li Zuxi <775717876@qq.com>
2021-03-26 18:07:43 +0800

    仪表盘添加氪金总数统计

--- yinjian-showtime/src/assets/js/pages/dashboard.init.js
+++ yinjian-showtime/src/assets/js/pages/dashboard.init.js
@@ -7,190 +7,202 @@ File: Dashboard Init Js File
 
 // Line-column chart
 
-// var options = {
-//     series: [{
-//         name: '2020',
-//         type: 'column',
-//         data: [23, 42, 35, 27, 43, 22, 17, 31, 22, 22, 12, 16]
-//     }, {
-//         name: '2019',
-//         type: 'line',
-//         data: [23, 32, 27, 38, 27, 32, 27, 38, 22, 31, 21, 16]
-//     }],
-//     chart: {
-//         height: 280,
-//         type: 'line',
-//         toolbar: {
-//             show: false,
-//         }
-//     },
-//     stroke: {
-//         width: [0, 3],
-//         curve: 'smooth'
-//     },
-//     plotOptions: {
-//         bar: {
-//             horizontal: false,
-//             columnWidth: '20%',
-//         },
-//     },
-//     dataLabels: {
-//         enabled: false,
-//     },
-//
-//     legend: {
-//         show: false,
-//     },
-//     colors: ['#5664d2', '#1cbb8c'],
-//     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-// };
-
-// var chart = new ApexCharts(document.querySelector("#line-column-chart"), options);
-// chart.render();
-
-var options = {
-    series: [
-        {
-            name: "虚拟",
-            type: "column",
-            data: [80345, 35865, 6989, 16895]
+$(document).ready(function() {
+    var dashBoardVue = new Vue({
+        el: '#kejinDashBoard',
+        data: {
+            hehe: false,
+            kejinList: [],
+            allMoney: 0,
+            allVirtual: 0,
+            allReality: 0
         },
-        {
-            name: "实体",
-            type: "column",
-            data: [13258, 68982, 6878, 4589]
+        methods: {
+            calculateAll() {
+                let length = this.kejinList.length;
+                let allMoney = 0;
+                let allVirtual = 0;
+                for (let i = 0; i < length; i++) {
+                    allMoney += Number(this.kejinList[i].money);
+                    if (this.kejinList[i].type == 0) {
+                        allVirtual += Number(this.kejinList[i].money);
+                    }
+                }
+                this.allMoney = allMoney;
+                this.allVirtual = allVirtual;
+                this.allReality = allMoney - allVirtual;
+                console.log(this.allMoney);
+            },
+        },
+        mounted() {
+            init();
+            this.calculateAll();
+        },
+        created() {
+            let thisPage = this;
+            $.ajax({
+                url: 'http://localhost:8080/buy/selectPage?pageIndex=1&pageSize=10000',
+                type: "GET",
+                async: false,
+                contentType: "text",
+                success: function(result) {
+                    console.log(result.data.records);
+                    thisPage.kejinList = result.data.records;
+                },
+                error: function(xMLHttpRequest, textStatus, errorThrown) {
+                }
+            });
         }
-    ],
-    chart: {height: 280, type: "line", toolbar: {show: !1}},
-    stroke: {width: [0, 0], curve: "smooth"},
-    plotOptions: {bar: {horizontal: !1, columnWidth: "60%"}},
-    dataLabels: {enabled: !1},
-    legend: {show: !1},
-    colors: ["#5664d2", "#1cbb8c"],
-    labels: ['洪哥', "路西", "拓哥", "阿鸡"]
-}, chart = new ApexCharts(document.querySelector("#line-column-chart"), options);
-chart.render();
+    })
 
-// donut chart
+});
 
-var options = {
-    series: [105431, 113568, 13524, 19869],
-    labels: ['洪哥', "路西", "拓哥", "阿鸡"],
-    chartOptions: {
-        labels: ['Apple', 'Mango', 'Orange', 'Watermelon']
-    },
-    chart: {
-        type: 'donut'
-        // type: 'pie'
-    },
-    dataLabels: {
-        enabled: true,
-        formatter: function (val) {
-            return val.toFixed(1) + "%"
-        }
-    },
-    plotOptions: {
-        pie: {
-            donut: {
-                size: '50%',
-                labels: {
-                    show: true,
-                    name: {
-                        show: true
-                    },
-                    value: {
-                        show: true
-                    },
-                    total: {
+function init() {
+    var options = {
+        series: [
+            {
+                name: "虚拟",
+                type: "column",
+                data: [80345, 35865, 6989, 16895]
+            },
+            {
+                name: "实体",
+                type: "column",
+                data: [13258, 68982, 6878, 4589]
+            }
+        ],
+        chart: {height: 280, type: "line", toolbar: {show: !1}},
+        stroke: {width: [0, 0], curve: "smooth"},
+        plotOptions: {bar: {horizontal: !1, columnWidth: "60%"}},
+        dataLabels: {enabled: !1},
+        legend: {show: !1},
+        colors: ["#5664d2", "#1cbb8c"],
+        labels: ['洪哥', "路西", "拓哥", "阿鸡"]
+    }, chart = new ApexCharts(document.querySelector("#line-column-chart"), options);
+    chart.render();
+
+    // donut chart
+
+    var options = {
+        series: [105431, 113568, 13524, 19869],
+        labels: ['洪哥', "路西", "拓哥", "阿鸡"],
+        chartOptions: {
+            labels: ['Apple', 'Mango', 'Orange', 'Watermelon']
+        },
+        chart: {
+            type: 'donut'
+            // type: 'pie'
+        },
+        dataLabels: {
+            enabled: true,
+            formatter: function (val) {
+                return val.toFixed(1) + "%"
+            }
+        },
+        plotOptions: {
+            pie: {
+                donut: {
+                    size: '50%',
+                    labels: {
                         show: true,
-                        label: '拢共',
-                        color: 'black'
+                        name: {
+                            show: true
+                        },
+                        value: {
+                            show: true
+                        },
+                        total: {
+                            show: true,
+                            label: '拢共',
+                            color: 'black'
+                        }
                     }
                 }
             }
         }
-    }
-};
+    };
 
-var chart = new ApexCharts(document.querySelector("#donut-chart"), options);
-chart.render();
+    var chart = new ApexCharts(document.querySelector("#donut-chart"), options);
+    chart.render();
 
 
-// Radialchart 1
+    // Radialchart 1
 
-var radialoptions = {
-    series: [72],
-    chart: {
-        type: 'radialBar',
-        wight: 60,
-        height: 60,
-        sparkline: {
-            enabled: true
-        }
-    },
-    dataLabels: {
-        enabled: false
-    },
-    colors: ['#5664d2'],
-    stroke: {
-        lineCap: 'round'
-    },
-    plotOptions: {
-        radialBar: {
-            hollow: {
-                margin: 0,
-                size: '70%'
-            },
-            track: {
-                margin: 0,
-            },
+    var radialoptions = {
+        series: [72],
+        chart: {
+            type: 'radialBar',
+            wight: 60,
+            height: 60,
+            sparkline: {
+                enabled: true
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        colors: ['#5664d2'],
+        stroke: {
+            lineCap: 'round'
+        },
+        plotOptions: {
+            radialBar: {
+                hollow: {
+                    margin: 0,
+                    size: '70%'
+                },
+                track: {
+                    margin: 0,
+                },
 
-            dataLabels: {
-                show: false
+                dataLabels: {
+                    show: false
+                }
             }
         }
-    }
-};
+    };
 
-var radialchart = new ApexCharts(document.querySelector("#radialchart-1"), radialoptions);
-radialchart.render();
+    var radialchart = new ApexCharts(document.querySelector("#radialchart-1"), radialoptions);
+    radialchart.render();
 
 
-// Radialchart 2
+    // Radialchart 2
 
-var radialoptions = {
-    series: [65],
-    chart: {
-        type: 'radialBar',
-        wight: 60,
-        height: 60,
-        sparkline: {
-            enabled: true
-        }
-    },
-    dataLabels: {
-        enabled: false
-    },
-    colors: ['#1cbb8c'],
-    stroke: {
-        lineCap: 'round'
-    },
-    plotOptions: {
-        radialBar: {
-            hollow: {
-                margin: 0,
-                size: '70%'
-            },
-            track: {
-                margin: 0,
-            },
+    var radialoptions = {
+        series: [65],
+        chart: {
+            type: 'radialBar',
+            wight: 60,
+            height: 60,
+            sparkline: {
+                enabled: true
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        colors: ['#1cbb8c'],
+        stroke: {
+            lineCap: 'round'
+        },
+        plotOptions: {
+            radialBar: {
+                hollow: {
+                    margin: 0,
+                    size: '70%'
+                },
+                track: {
+                    margin: 0,
+                },
 
-            dataLabels: {
-                show: false
+                dataLabels: {
+                    show: false
+                }
             }
         }
-    }
-};
+    };
 
-var radialchart = new ApexCharts(document.querySelector("#radialchart-2"), radialoptions);
-radialchart.render();
+    var radialchart = new ApexCharts(document.querySelector("#radialchart-2"), radialoptions);
+    radialchart.render();
+
+}
